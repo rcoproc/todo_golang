d7f3367ee65437559d22cd6c2790df6dbd6f1829
Ricardo César de Oliveira <rioliveira@bionexo.com>
2021-05-09 00:34:31 -0300

    Feat: Added Stdin input -add option

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"rcoproc.com/rcoproc/todo"
-	_ "strings"
+	"strings"
 )
 
 // Default file name
@@ -13,16 +15,24 @@ var todoFileName = ".todo.json"
 
 func main() {
 	// Parsing command line flags
-	task := flag.String("task", "", "Task to be included in the Todo List")
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for the Ricard Oliveira Tecnologia\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2021\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage Information: ")
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
-  // Check if the user defined the ENV VAR for a custom file name
-  if os.Getenv("TODO_FILENAME") != "" {
-    todoFileName = os.Getenv("TODO_FILENAME")
-  }
+	// Check if the user defined the ENV VAR for a custom file name
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
 
 	// Define an items list
 	l := &todo.List{}
@@ -56,9 +66,17 @@ func main() {
 			os.Exit(1)
 		}
 
-	case *task != "":
-		// Add the task
-		l.Add(*task)
+	case *add:
+		// When any arguments (excluding flags) are provided, they will be
+		// used as new task
+
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		l.Add(t)
 
 		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
@@ -67,4 +85,22 @@ func main() {
 		}
 	}
 
+}
+
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task canno be blank")
+	}
+
+	return s.Text(), nil
 }
